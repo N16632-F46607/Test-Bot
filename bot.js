690c9fb6287be39e661daff79b9c0fb6fc2e4fd3
N16632-F46607 <53200157+N16632-F46607@users.noreply.github.com>
2019-07-24 13:48:50 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,14 +13,14 @@ const { Client, RichEmbed } = require('discord.js');
 const prefix = '|';
 // Importa el modulo de File System para escribir, leer, abrir, cerrar archivos
 const fs = require ('fs');
-// Datos/Valores almacenados en el archivo data.json
-const value = require('./data.json');
-// Contador de baneos
-var count = value.banCount;
+var data = require('./data.json');
+
+var messagePurpose = '';
+var skdValue = '';
 
 /**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
+ * El evento ready es vital, sólamente _después_ de este tu bot podrá empezar a reaccionar
+ * a la información recibida de Discord
  */
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -29,40 +29,58 @@ client.on('ready', () => {
 
 // Crea un event listener para los mensajes
 client.on('message', message => {
+	if (!(message.channel.type === 'dm')) {
+		
+		setLastCommandRequested('');
+	}
 	// Si no se utiliza el prefijo se ignora el mensaje
 	if (!message.content.startsWith(prefix)) return;
-	message.content = message.content.substring(prefix.length);
+	message.content = message.content.toLowerCase().substring(prefix.length);
 
-  	if (message.content.toLowerCase() === 'help') {
-		const embed = new RichEmbed()
-			.setTitle('Lista de Comandos:')
-			.setColor(0x1a1c2e)
-			.setDescription(prefix + 'dahir: Contador de expulsiones del Dahir \n' +
-				prefix + 'sdk #: Establece el contador de expulsiones al número escrito');
-		message.channel.send(embed);
+	if (message.channel.type === 'text') {
+
+		if (message.content === 'help') {
+			const embed = new RichEmbed()
+				.setTitle('📄 Lista de Comandos:')
+				.setDescription('`' + prefix + 'dahir`: Contador de expulsiones del Dahir \n'
+							  + '`' + prefix + 'sdk #`: Establece el contador de expulsiones al número escrito')
+				.setColor(0x1a1c2e)
+			message.channel.send(embed);
+		} else
+
+	  	if (message.content === 'dahir') {
+			const embed = new RichEmbed()
+				.setTitle('Dahir')
+				.setDescription('Expulsado: ' + getKicksQty() + ' Veces!')
+				.setColor(0xe62c44);
+			message.channel.send(embed);
+		} else
+
+		if (message.content.startsWith('sdk')) {
+			if (!(message.content.match(new RegExp('sdk [0-9]+$', 'i')))) return;
+			setLastCommandRequested('sdk');
+			skdValue = message.content.substring(4);
+			const embed = new RichEmbed()
+				.setTitle('🔒 Autorización Requerida')
+				.setDescription('Para ejecutar el comando: `' + prefix + getLastCommandRequested() + '` '
+								+ 'es requerida una contraseña.\n'
+								+ '`' + prefix + 'pass`: Comando para la contraseña')
+				.setColor(0x1a1c2e);
+			message.author.send(embed);
+		}
 	} else
 
-  	if (message.content.toLowerCase() === 'dahir') {
-		const embed = new RichEmbed()
-			.setTitle('Dahir')
-			.setColor(0xe62c44)
-			.setDescription('Expulsado: ' + count + ' Veces!');
-		message.channel.send(embed);
-	} else
-
-	if (message.content.toLowerCase().startsWith('sdk ')) {
-		if (isNaN(message.content.substring(4)) || message.content.substring(4) === '' || message.content.substring(4).startsWith('-')) {
-			message.channel.send('Error!');
-			return;
-		} else {
-			count = message.content.substring(4);
-			let data = { 
-				banCount: count
-			};
-			fs.writeFile('./data.json', JSON.stringify(data, null, 4), (err) => { 
-				if (err) throw err;
-			});
-			message.channel.send('Listo!')
+	if (message.channel.type === 'dm') {
+		if (message.content.startsWith('pass')) {
+			if (getLastCommandRequested() === 'sdk') {
+				if (message.content.substring(5) === 'nimda') {
+					message.react('✔');
+					setKicksQty(skdValue);
+					setLastCommandRequested('');
+				} else {
+					message.react('✖');
+				}
+			}
 		}
 	}
 });
@@ -70,20 +88,34 @@ client.on('message', message => {
 // Crea un event listener para las expulsiones
 client.on('guildMemberRemove', member => {
 	// Si es el Dahir
-	if (member.user.discriminator == '4811') {
+	if (member.user.id == '307592698385399810') {
 		// Lo suma al contador
-		count += 1;
-		let data = { 
-			banCount: count
-		};
-		// Y lo almacena en el archivo data.json
-		fs.writeFile('./data.json', JSON.stringify(data, null, 4), (err) => { 
-			if (err) throw err;
-		});
+		setKicksQty(getKicksQty() + 1);
 	}
-	// En cualquier caso envía una despedida
-	client.channels.get('470938434500820996').send('Al rato @' + member.user.username + '!');
+	// En cualquier caso envía un mensaje de despedida
+	client.channels.get('602945989556305970').send('Al rato @' + member.user.username + '!');
 });
 
 // Logea al bot utilizando el token del archivo auth.json de https://discordapp.com/developers/applications/me
 client.login(process.env.BOT_TOKEN);
+
+//Funciones
+
+function getKicksQty() {
+	return data.banCount;
+}
+
+function setKicksQty(quantity) {
+	data = { banCount: quantity };
+	fs.writeFile('./data.json', JSON.stringify(data, null, 4), err => { 
+		if (err) throw err;
+	});
+}
+
+function getLastCommandRequested() {
+	return messagePurpose;
+}
+
+function setLastCommandRequested(command) {
+	messagePurpose = command.substring(command.indexOf(prefix) + 1);
+}
