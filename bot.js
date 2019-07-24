becbcd7afaf6fa3c97d5560f58c0f68f781dc5a7
N16632-F46607 <53200157+N16632-F46607@users.noreply.github.com>
2019-07-23 17:55:14 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,75 @@
+// Archivo de donde el Token será tomado
+const auth = require('./auth.json');
+// Importa el modulo discord.js
+const Discord = require('discord.js');
+// Crea una instancia de un cliente de Discord
+const client = new Discord.Client();
+// Extrae las clases requeridas del modulo discord.js
+const { Client, RichEmbed } = require('discord.js');
+
+// Atributos (Otros)
+
+// Prefijo para los comandos
+const prefix = '|';
+// Importa el modulo de File System para escribir, leer, abrir, cerrar archivos
+const fs = require ('fs');
+// Datos/Valores almacenados en el archivo data.json
+const value = require('./data.json');
+// Contador de baneos
+var count = value.banCount;
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Crea un event listener para los mensajes
+client.on('message', message => {
+	// Si no se utiliza el prefijo se ignora el mensaje
+	if (!message.content.startsWith(prefix)) return;
+	message.content = message.content.substring(prefix.length);
+
+  	if (message.content.toLowerCase() === 'help') {
+		const embed = new RichEmbed()
+			.setTitle('Lista de Comandos:')
+			.setColor(0x1a1c2e)
+			.setDescription(prefix + 'dahir: Contador de expulsiones del Dahir');
+		message.channel.send(embed);
+	} else
+
+  	if (message.content.toLowerCase() === 'dahir') {
+		const embed = new RichEmbed()
+			.setTitle('Dahir')
+			.setColor(0x343050)
+			.setDescription('Baneado: ' + count + ' Veces!');
+		message.channel.send(embed);
+	} //else
+
+	//if (message.content.toLowerCase() === 'prueba') {
+	//	const auditLogs = new GuildAuditLogs;
+	//}
+});
+
+// Crea un event listener para las expulsiones
+client.on('guildMemberRemove', member => {
+	// Si es el Dahir
+	if (member.user.discriminator == '4811') {
+		// Lo suma al contador
+		count += 1;
+		let data = { 
+			banCount: count
+		};
+		// Y lo almacena en el archivo data.json
+		fs.writeFile('./data.json', JSON.stringify(data, null, 4), (err) => { 
+			if (err) throw err;
+		});
+	}
+	// En cualquier caso envía una despedida
+	client.channels.get('602945989556305970').send('Al rato @' + member.user.username + '!');
+});
+
+// Logea al bot utilizando el token del archivo auth.json de https://discordapp.com/developers/applications/me
+client.login(auth.token);
